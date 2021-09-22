ee766f36a3dd87c7d48694252fcd2cf47c2aea5d
ADeBu99 <adrian.asuncion@ceiba.com.co>
2021-09-21 23:28:29 -0500

    Task => 3. Closure and loops

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,16 @@
+const createClassRoom = (numbersOfStudents) => {
+  const studentSeat = (seat) => {
+    return () => {
+      return seat;
+    }
+  }
+  let students = [];
+
+  for(i = 0; i < numbersOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  };
+
+  return students;
+}
+
+const classRoom = createClassRoom(10);
