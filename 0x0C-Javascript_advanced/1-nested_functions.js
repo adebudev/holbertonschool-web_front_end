d98bdd117469b08100ff4c0dd2f42c75b6ab5f10
ADeBu99 <adrian.asuncion@ceiba.com.co>
2021-09-21 22:57:37 -0500

    Task => 1. Closure Scope Chain

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,20 @@
+let globalVariable = "Welcome";
+
+const outer = () => {
+  alert(globalVariable)
+  let course = "Holberton";
+
+  const inner = () => {
+    alert(`${globalVariable} ${course}`);
+    let exclamation = '!';
+
+    const inception = () => {
+      alert(`${globalVariable} ${course}${exclamation}`);
+    }
+    inception();
+  }
+  inner();
+}
+
+// call function
+outer();
